d329c463aa038f4e7b0b8877d95ac19b01293e17
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2014-11-19 16:07:09 +0800

    added required libraries for ruby to compile in ubuntu 14.04

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,15 @@ package 'libcurl3'
 package 'libcurl3-dev'
 package 'libmagickwand-dev'
 package 'imagemagick'
+package 'autoconf'
+package 'bison'
+package 'libssl-dev'
+package 'libyaml-dev'
+package 'libreadline6'
+package 'libreadline6-dev'
+package 'zlib1g'
+package 'zlib1g-dev'
+
 apt_repository("node.js") do
   uri "http://ppa.launchpad.net/chris-lea/node.js/ubuntu"
   distribution node['lsb']['codename']
